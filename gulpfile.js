80bb9605b4e1c0e71fcd4375818798ef822de2c8
Alexandra Caulea <alexandra.caulea@gmail.com>
2019-10-28 10:10:59 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const gulp = require("gulp");
+const browserSync = require("browser-sync").create();
+const sass = require("gulp-sass");
+const prefix = require("gulp-autoprefixer");
+
+// compile scss into css
+function scssTask() {
+  return gulp
+    .src("./scss/**/*.scss")
+    .pipe(sass())
+    .pipe(prefix("last 5 versions"))
+    .pipe(gulp.dest("./css"))
+    .pipe(browserSync.stream());
+}
+
+// Browser Sync
+function watchTask() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+  gulp.watch("./scss/**/*.scss", scssTask);
+  gulp.watch("./*.html").on("change", browserSync.reload);
+}
+
+exports.scssTask = scssTask;
+exports.watchTask = watchTask;
